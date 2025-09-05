3bc323c5a9548947f16777933156532a3e45f3be
gvz01 <angeldivinester@gmail.com>
2025-09-05 17:59:29 +0530

    Add files via upload
    Basic Mathematical Operation to add, subtract and multiply 2 numbers.

---
+++ Assignment_1_Task_1.py
@@ -0,0 +1,11 @@
+a=input('please input first num:' )
+a=int(a)
+b=input('please input second num:')
+b=int(b)
+sum = a+b
+print(sum)
+subtract = a-b
+print(subtract)
+multiplication = a*b
+print(multiplication)
+
