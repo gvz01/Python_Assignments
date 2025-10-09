a95344f96b779bd096583fd611c1cd35b8363901
gvz01 <angeldivinester@gmail.com>
2025-10-09 18:58:29 +0530

    Add files via upload
     Using the Math Module for Calculations

---
+++ Assignment3_Math Module for Calculation.py
@@ -0,0 +1,11 @@
+import math
+
+num = float(input("Enter a positive number: "))
+
+sqrt_val = math.sqrt(num)
+ln_val = math.log(num)
+sin_val = math.sin(num)
+
+print("Square root:", sqrt_val)
+print("Natural logarithm (base e):", ln_val)
+print("Sine (radians):", sin_val)
